a212f30b4453e9eb9c2930e4887ef5b33348bb38
acs1899 <yrn19901005@gmail.com>
2015-04-17 20:21:06 +0800

    fix no dir

--- phan.sh
+++ phan.sh
@@ -6,57 +6,63 @@ ACTION=$1
 
 #help
 usage(){
-    echo "Usage: ./phan.sh {start|stop|restart}"
-    exit 1;
+  echo "Usage: ./phan.sh {start|stop|restart}"
+  exit 1;
 }
 
 get_pid(){
-    if [ -f ./run/app.pid ]; then
-        echo `cat ./run/app.pid`
-    fi
+  if [ ! -d $DIR/run ]; then
+    mkdir $DIR/run
+  fi
+  if [ ! -d $DIR/log ]; then
+    mkdir $DIR/log
+  fi
+  if [ -f $DIR/run/app.pid ]; then
+    echo `cat ./run/app.pid`
+  fi
 }
 
 #start app
 start(){
-    pid=`get_pid`
-    if [ ! -z $pid ]; then
-        echo 'server is already running'
-    else
-        $NODE $DIR/index.js >$DIR/logs/phan.log 2>&1 &
-        echo 'server is running'
-    fi
+  pid=`get_pid`
+  if [ ! -z $pid ]; then
+    echo 'server is already running'
+  else
+    $NODE $DIR/index.js >$DIR/logs/phan.log 2>&1 &
+    echo 'server is running'
+  fi
 }
 
 #stop app
 stop(){
-    pid=`get_pid`
-    if [ -z $pid ]; then
-        echo 'server not running'
-    else
-        echo 'server is stopping...'
-        kill -15 $pid
-        echo 'server stopped'
-    fi
+  pid=`get_pid`
+  if [ -z $pid ]; then
+    echo 'server not running'
+  else
+    echo 'server is stopping...'
+    kill -15 $pid
+    echo 'server stopped'
+  fi
 }
 
 restart(){
-    stop
-    sleep 0.5
-    echo =====
-    start
+  stop
+  sleep 0.5
+  echo =====
+  start
 }
 
 case "$ACTION" in
-    start)
-        start
-    ;;
-    stop)
-        stop
-    ;;
-    restart)
-        restart
-    ;;
-    *)
-        usage
-    ;;
+  start)
+    start
+  ;;
+  stop)
+    stop
+  ;;
+  restart)
+    restart
+  ;;
+  *)
+    usage
+  ;;
 esac
